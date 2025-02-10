bfe01a3735d245161c3759c20f7dbaf7971f3f26
Rahul <147478589+iitian12@users.noreply.github.com>
2025-02-10 13:57:28 +0530

    added vercel api

--- src/backendApi/api.js
+++ src/backendApi/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://learn-hub-backend-u8g4.vercel.app";
 
 export const getCourses = async () => {
     try {
